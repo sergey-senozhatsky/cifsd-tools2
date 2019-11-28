d10d79af8aaf78fc8a7661524e433130435429b7
Namjae Jeon <linkinjeon@gmail.com>
2019-11-27 20:39:40 -0500

    cifsd-tools: remove unused getopt arguments(W and ;)
    Signed-off-by: Namjae Jeon <linkinjeon@gmail.com>

--- cifsd/cifsd.c
+++ cifsd/cifsd.c
@@ -445,7 +445,7 @@ int main(int argc, char *argv[])
 
 	opterr = 0;
 	while (1) {
-		c = getopt_long(argc, argv, "n::p:c:u:sVhW;", opts, NULL);
+		c = getopt_long(argc, argv, "n::p:c:u:sVh", opts, NULL);
 
 		if (c < 0)
 			break;
